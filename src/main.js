a7dc021bb9d7d288a3c8ea90fa51489d823570c7
Micheal Hansen <micheal.hansen@gmail.com>
2020-07-02 15:41:12 -0700

    add UI formatting
    Co-authored-by: Brittany Lindgren <brittdevelopsit@gmail.com>

--- src/main.js
+++ src/main.js
@@ -28,32 +28,34 @@ $(document).ready(function() {
     const row1 = $("#alphabetRow1");
     const row2 = $("#alphabetRow2");
     const row3 = $("#alphabetRow3");
-    let row1HTML = `<div class='row'>`;
+    let row1HTML = `<div class='row no-gutters'>`;
     for (let i=0; i < 12; i++) {
       row1HTML += `<div class='col-md-1'><button class="btn btn-info alphaButton" id='alphabetButton${alphabet[i]}'>${alphabet[i]}</button></div>`;
     }
     row1HTML += `</div>`;
-    let row2HTML = `<div class='row'>`;
+    let row2HTML = `<div class='row no-gutters'>`;
     for (let i=12; i < 24; i++) {
       row2HTML += `<div class='col-md-1'><button class="btn btn-info alphaButton" id='alphabetButton${alphabet[i]}'>${alphabet[i]}</button></div>`;
     }
     row2HTML += '</div>';
-    let row3HTML = '<div class="row">';
+    let row3HTML = '<div class="row no-gutters"><div class="col-md-5"></div>';
     for (let i = 24; i < 26; i++) {
       row3HTML += `<div class='col-md-1'><button class="btn btn-info alphaButton" id='alphabetButton${alphabet[i]}'>${alphabet[i]}</button></div>`;
     }
-    row3HTML += '</div>';
+    row3HTML += '<div class="col-md-5"></div></div>';
     row1.html(row1HTML);
     row2.html(row2HTML);
     row3.html(row3HTML);
 
-    $("#remainingGuesses").html(`<p>You have <span id='guesses'>${board.guessesRemaining}</span> guesses remaining until an asteroid hits the Earth</p>`);
+    $("#remainingGuesses").html(`<p>You have <span id='guesses'>${board.guessesRemaining}</span> guesses remaining until an asteroid hits the Earth!</p>`);
 
     $(".alphaButton").click(function() {
       const letterToReplace = $(this).attr("id").slice(14);
       board.makeGuess(letterToReplace);
       updateDisplay(board);
       $("#guesses").text(board.guessesRemaining);
+      $(this).addClass('disabled');
+      $(this).off('click');
       if (board.gameStatus() === 'win') {
         winner();
         clearDisplay();
