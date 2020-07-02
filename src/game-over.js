23d0d4ee2b443bbb2e73632f75615e1a6afb6c31
Micheal Hansen <micheal.hansen@gmail.com>
2020-07-02 16:15:25 -0700

    fix a typo
    Co-authored-by: Brittany Lindgren <brittdevelopsit@gmail.com>

--- src/game-over.js
+++ src/game-over.js
@@ -2,21 +2,21 @@ import $ from 'jquery';
 import { dinoWinnerGif, dinoLoserGif } from './dinoGIF';
 
 export function winner() {
-  $("#gameOver").html(`<p>You Win!</p><span id='#dinoGIF'></span>`);
+  $("#gameOver").html(`<p>You Win!</p><span id='dinoGIF'></span>`);
   (async () => {
     const dinoGIF = await dinoWinnerGif();
     if (dinoGIF) {
-      $('#dinoGIF').html(`<img src=${dinoGIF} alt='A VICTORIOUS DINOSAUR'>`);
+      $('#dinoGIF').html(`<img src='${dinoGIF}' alt='A VICTORIOUS DINOSAUR'>`);
     }
   })();
 }
 
 export function loser() {
-  $("#gameOver").html(`<p>You Lose!</p><span id='#meteorGIF'></span>`);
+  $("#gameOver").html(`<p>You Lose!</p><span id='meteorGIF'></span>`);
   (async () => {
     const meteorGIF = await dinoLoserGif();
     if (meteorGIF) {
-      $('#meteorGIF').html(`<img src=${meteorGIF} alt='YOU WENT EXTINCT'>`);
+      $('#meteorGIF').html(`<img src='${meteorGIF}' alt='YOU WENT EXTINCT'>`);
     }
   })();
 }
