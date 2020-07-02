21ab23f809eee31410006d443a8cdd657f6fddea
Micheal Hansen <micheal.hansen@gmail.com>
2020-07-02 15:58:45 -0700

    add dino gif
    Co-authored-by: Brittany Lindgren <brittdevelopsit@gmail.com>

---
+++ src/dinoGIF.js
@@ -0,0 +1,18 @@
+export async function dinoWinnerGif() {
+  let response = await fetch(`https://api.giphy.com/v1/gifs/search?api_key=${process.env.API_KEY}&q=dinosaur&limit=1&offset=0&rating=G&lang=en`);
+  if (!response.ok) {
+    return false;
+  }
+  const responseJSON = response.json();
+  return responseJSON["data"][0]["images"]["original"]["url"];
+}
+
+
+export async function dinoLoserGif() {
+  let response = await fetch(`https://api.giphy.com/v1/gifs/search?api_key=${process.env.API_KEY}&q=meteor&limit=1&offset=0&rating=G&lang=en`);
+  if (!response.ok) {
+    return false;
+  }
+  const responseJSON = response.json();
+  return responseJSON["data"][0]["images"]["original"]["url"];
+}
